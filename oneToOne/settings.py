d4fa93c8e487829698afa05c12cefcea600aad00
Subrat Thayal <57702457+subratthayal@users.noreply.github.com>
2020-02-05 18:36:39 +0530

    Update settings.py

--- oneToOne/settings.py
+++ oneToOne/settings.py
@@ -125,6 +125,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-ENABLEX_API_URL = "https://api.vcloudx.com/v1"
+ENABLEX_API_URL = "https://api.enablex.io/v1"
 ENABLEX_APP_ID = ""
 ENABLEX_APP_KEY = ""
